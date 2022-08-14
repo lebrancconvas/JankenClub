9a983c0ee2c90952801443414ba185bd96bfae77
Poom Yimyuean <33986491+lebrancconvas@users.noreply.github.com>
2022-08-14 14:27:30 +0700

    Initial commit

---
+++ apps/web/src/routes/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<title>MESN Turbo</title>
+</svelte:head>
+
+<main>
+	<header>
+		<div>
+			<h1>MESN Turbo</h1>
+		</div>
+	</header>
+</main>
+
+<style>
+
+</style>
