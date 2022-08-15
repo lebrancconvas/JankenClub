a99bb2e15f0ee9e7da51331a3ae13e16989fd18e
lebrancconvas <lebrancconvas@gmail.com>
2022-08-15 13:37:27 +0700

    ✨ feat: create login logic

--- apps/web/src/routes/index.svelte
+++ apps/web/src/routes/index.svelte
@@ -1,11 +1,7 @@
 <script lang="ts">
 	import JankenCard from '../components/JankenCard.svelte';
-	type Hand = "ROCK" | "PAPER" | "SCISSOR"; 
-	let hand: Hand;
 
-	const handleClick = ():void => {
-		alert(`You selected ${hand}`);
-	};
+	let isLoggedin: boolean = false;
 </script>
 
 <svelte:head>
@@ -18,11 +14,19 @@
 			<h1>Janken Club</h1>
 		</div>
 	</header>
-	<section id="canvas">
-		<JankenCard hand="Rock" img="https://cdn-icons.flaticon.com/png/512/3562/premium/3562093.png?token=exp=1660465425~hmac=8f546422a5c6a4f52b3619a6a4682964" />
-		<JankenCard hand="Paper" img="https://cdn-icons-png.flaticon.com/512/2910/2910908.png" />
-		<JankenCard hand="Scissors" img="https://cdn-icons.flaticon.com/png/512/4151/premium/4151732.png?token=exp=1660465628~hmac=83d19677fb272fc38e456ac4b52f2546" />
-	</section>
+	{#if isLoggedin}
+		<section id="canvas">
+				<JankenCard hand="Rock" img="https://cdn-icons.flaticon.com/png/512/3562/premium/3562093.png?token=exp=1660465425~hmac=8f546422a5c6a4f52b3619a6a4682964" />
+				<JankenCard hand="Paper" img="https://cdn-icons-png.flaticon.com/512/2910/2910908.png" />
+				<JankenCard hand="Scissors" img="https://cdn-icons.flaticon.com/png/512/4151/premium/4151732.png?token=exp=1660465628~hmac=83d19677fb272fc38e456ac4b52f2546" />
+		</section>
+	{:else}
+		<section id="canvas-not">
+			<div>
+				<h1>You don't have an access to the website</h1>
+			</div>
+		</section>
+	{/if}
 </main>
 
 <style>
@@ -42,5 +46,9 @@
 		grid-template-columns: 40% 40% 40%;
 		text-align: center;
 		margin: 0 auto;
+	}
+
+	#canvas-not {
+		text-align: center;
 	}
 </style>
---
+++ apps/web/src/routes/login/index.svelte
@@ -0,0 +1,69 @@
+<script lang="ts">
+
+</script>
+
+<svelte:head>
+	<title>Login | Janken Club</title>
+</svelte:head>
+
+<main>
+	<header>
+		<div>
+			<h1>Login</h1>
+		</div>
+	</header>
+	<section id="login-canvas">
+		<div id="login-card">
+			<form action="">
+				<label for="">Username</label>
+				<br>
+				<input type="text" name="username" id="login-username">
+				<br>
+				<button type="submit">Login</button>
+			</form>
+		</div>
+	</section>
+</main>
+
+<style>
+	* {
+		font-family: "Bungee", cursive;
+	}
+
+	header {
+		text-align: center;
+	}
+
+	#login-canvas {
+		/* border: 2px solid red; */
+		height: 500px;
+	}
+
+	#login-card {
+		border: 2px solid black;
+		height: 80%;
+		width: 50%;
+		margin: 10px auto; 
+		padding: 10px; 
+		border-radius: 10px;
+		box-shadow: 1px 1px 1px 1px black;
+		text-align: center;
+		background-color: rgb(255,192,96);
+		background-color: linear-gradient(90deg, rgba(255,192,96,1) 37%, rgba(228,178,50,1) 80%);
+	}
+
+	label {
+		font-size: 30px;
+		margin: 20px;
+	}
+
+	input {
+		font-size: 23px;
+		outline: 0;
+		border-width: 0 0 2px;
+	}
+
+	button {
+		margin: 10px 0;
+	}
+</style>
